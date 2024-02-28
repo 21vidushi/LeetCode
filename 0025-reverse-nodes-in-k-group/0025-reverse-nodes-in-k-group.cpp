c5d18fd483850d3ffb39ce3bb5a13ecf31b634e5
Vidushi Singhal <104440693+21vidushi@users.noreply.github.com>
2024-02-28 11:06:00 +0530

    Time: 7 ms (88.59%), Space: 15 MB (51.91%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,34 +10,37 @@
  */
 class Solution {
 public:
-    int find(ListNode* &head){
-        ListNode*temp=head;
-        int cnt=0;
-        while(temp!=NULL){
-            cnt++;
-            temp=temp->next;
-        }
-        return cnt;
-    }
     ListNode* reverseKGroup(ListNode* head, int k) {
-       if(head==NULL||head->next==NULL) return head;
-        int len=find(head);
-        if(len<k) return head;
-        ListNode* temp=head;
-        ListNode* prev=NULL;
-        ListNode* curr=NULL;
-        int cnt=0;
-        while(cnt<k){
-            curr=temp->next;
-            temp->next=prev;
-            prev=temp;
-            temp=curr;
-            cnt++;
+        ListNode* current = head;
+        int count = 0;
+        // Count the number of nodes in the current group
+        while (current != nullptr && count != k) {
+            current = current->next;
+            count++;
         }
-        if(temp!=NULL){
-            head->next=reverseKGroup(temp,k);
+        // If the number of nodes in the group is less than k, no need to reverse
+        if (count < k) {
+            return head;
+        }
+        // Reverse the current group
+        ListNode* reversedHead = reverseLinkedList(head, k);
+        // Recursively call for the remaining list
+        head->next = reverseKGroup(current, k);
+        return reversedHead;
+    }
+    
+private:
+    // Function to reverse a linked list of size k
+    ListNode* reverseLinkedList(ListNode* head, int k) {
+        ListNode* prev = nullptr;
+        ListNode* current = head;
+        ListNode* next = nullptr;
+        for (int i = 0; i < k; i++) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
         }
         return prev;
-        
     }
 };
