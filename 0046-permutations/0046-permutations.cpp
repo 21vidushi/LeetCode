91403b8706c215f45f05d204f05378e708bf9991
Vidushi Singhal <104440693+21vidushi@users.noreply.github.com>
2024-04-19 18:15:19 +0530

    Time: 5 ms (32.63%), Space: 8.9 MB (59.11%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
-    void find(vector<int>& nums, vector<int>& temp,vector<int>&mpp,vector<vector<int>> &ans){
+    void find(vector<int>& nums, vector<int>& temp,vector<vector<int>> &ans){
         if(temp.size()==nums.size()){
             ans.push_back(temp); return;
         }
         for(int i=0;i<nums.size();i++){
-            if(mpp[i]==0){
+            if(::find(temp.begin(),temp.end(),nums[i])==temp.end()){
                 temp.push_back(nums[i]);
-                mpp[i]=1;
-                find(nums,temp,mpp,ans);
-                mpp[i]=0;
+                find(nums,temp,ans);
                 temp.pop_back();
             }
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int>mpp(nums.size(),0);
         vector<int> temp;
-        find(nums,temp,mpp,ans);
+        find(nums,temp,ans);
         return ans;
         
     }
