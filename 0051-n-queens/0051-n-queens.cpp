143179d88f7c68411448a70ec25d95e7368ed699
Vidushi Singhal <104440693+21vidushi@users.noreply.github.com>
2024-04-20 21:21:55 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (81.77%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,51 +1,37 @@
 class Solution {
 public:
-    bool isSafe(int row,int col,vector<string> board,int n){
-        int row1=row;
-        int col1=col;
-         while(row>=0&&col>=0){
-            if(board[row][col]=='Q') return false;
-            row--;
-             col--;
-        }
-        row=row1;
-        col=col1;
-        while(col>=0){
-            if(board[row][col]=='Q') return false;
-            col--;
-        }
-
-       
-        row=row1;
-        col=col1;
-        while(row<n&&col>=0){
-             if(board[row][col]=='Q') return false;
-             col--;
-            row++;
-        }
-        return true;
-    }
-    void solve( vector<vector<string>> &ans,int n, int col,vector<string> &board){
+    void solve( vector<vector<string>> &ans,int n, int col,vector<string> &board,vector<int>&row,vector<int>& lowerD,vector<int> &upperD){
         if(col==n){
             ans.push_back(board);
             return;
         }
         for(int i=0;i<n;i++){
-            if(isSafe(i,col,board,n)){
+            if(row[i]==0&&lowerD[i+col]==0&&upperD[n-1+col-i]==0){
             board[i][col]='Q';
-            solve(ans,n,col+1,board);
+            row[i]=1;
+            lowerD[i+col]=1;
+            upperD[n-1+col-i]=1;
+            solve(ans,n,col+1,board,row,lowerD,upperD);
             board[i][col]='.';
-        }
+             row[i]=0;
+            lowerD[i+col]=0;
+            upperD[n-1+col-i]=0;
+            }
+            
+            
         }
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
         vector<string> board(n);
+        vector<int> row(n,0);
+        vector<int> lowerD(2*n-1,0);
+         vector<int> upperD(2*n-1,0);
         string s(n,'.');
         for(int i=0;i<n;i++){
             board[i]=s;
         }
-        solve(ans,n,0,board);
+        solve(ans,n,0,board,row,lowerD,upperD);
         return ans;
     }
 };
