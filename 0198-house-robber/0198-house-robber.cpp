14c44fc73de456a45b0c872dfb376176edf61bae
Vidushi Singhal <104440693+21vidushi@users.noreply.github.com>
2024-04-23 21:44:34 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (58.69%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int i ,vector<int>&dp){
-        if(i<0) return 0;
-        if(i==0) return nums[0];
-        if(dp[i]!=-1) return dp[i];
-        int pick= nums[i]+solve(nums,i-2,dp);
-        int nott= solve(nums,i-1,dp);
-        return dp[i]=max(pick,nott);
-    }
+    // int solve(vector<int>& nums,int i ,vector<int>&dp){
+    //     if(i<0) return 0;
+    //     if(i==0) return nums[0];
+    //     if(dp[i]!=-1) return dp[i];
+    //     int pick= nums[i]+solve(nums,i-2,dp);
+    //     int nott= solve(nums,i-1,dp);
+    //     return dp[i]=max(pick,nott);
+    // }
     int rob(vector<int>& nums) {
         int n=nums.size();
        vector<int>dp(n,0);
-        dp[0]=nums[0];
+        int prev=nums[0];
+        int prev1=0;
         if(n>1){
-        dp[1]=max(nums[0],nums[1]); }
+         prev1=max(nums[0],nums[1]); }
         for(int i=2;i<n;i++){
             
-            int pick= nums[i]+dp[i-2]; 
-            int nott= dp[i-1];
-            dp[i]=max(pick,nott);
+            int pick= nums[i]+prev; 
+            int nott= prev1;
+            int curr=max(pick,nott);
+            prev=prev1;
+            prev1=curr;
         }
-        return dp[n-1];
+       if(n==1) return prev;
+        else return prev1;
     }
 };
