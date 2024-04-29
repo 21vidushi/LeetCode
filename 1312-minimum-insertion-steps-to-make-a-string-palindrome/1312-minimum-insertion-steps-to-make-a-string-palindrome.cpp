5d75968e4729affcdbac9e0b19cacefa5d19a150
Vidushi Singhal <104440693+21vidushi@users.noreply.github.com>
2024-04-29 14:37:00 +0530

    Time: 25 ms (70.14%), Space: 8 MB (90.64%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
-    int find(string&s1,string&s2){
-        int n=s1.size(),m=s2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=0;
-        }
-         for(int i=0;i<=m;i++){
-            dp[0][i]=0;
-        }
+    int minInsertions(string s) {
+        string t=s;
+        reverse(t.begin(),t.end());
+        int n=s.size(),m=t.size();
+        vector<int> prev(m+1,0),curr(m+1,0);
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s1[i-1]==s2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-                }
+                if(s[i-1]==t[j-1]) curr[j]=1+prev[j-1];
+                else curr[j]=max(prev[j],curr[j-1]);
             }
+            prev=curr;
         }
-        return dp[n][m];
-    }
-    int minInsertions(string s) {
-        int n=s.length();
-        string s1=s;
-        reverse(s.begin(),s.end());
-        string s2=s;
-        int ans=find(s1,s2);
-        return n-ans;
+        return n-prev[m];
     }
 };
