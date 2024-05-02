caadf8b02f817405298e215cbfb653e01b13a6dc
Vidushi Singhal <104440693+21vidushi@users.noreply.github.com>
2024-05-03 01:27:31 +0530

    Time: 32 ms (53.71%), Space: 20.2 MB (37.82%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -18,23 +18,24 @@ public:
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        vector<int>prev(amount+1,0),curr(amount+1,0);
         for(int a=1;a<=amount;a++){
-            if(a%coins[0]==0) dp[0][a]=a/coins[0];
-            else dp[0][a]=1e9;
+            if(a%coins[0]==0) prev[a]=a/coins[0];
+            else prev[a]=1e9;
         }
         for(int i=1;i<n;i++){
             for(int a=1;a<=amount;a++){
                 int pick=1e9;
-                int nott=dp[i-1][a];
+                int nott=prev[a];
                 if(coins[i]<=a){
-                    pick=1+dp[i][a-coins[i]];
+                    pick=1+curr[a-coins[i]];
                 }
-                dp[i][a]=min(pick,nott);
+                curr[a]=min(pick,nott);
             }
+            prev=curr;
         }
         
-        int ans= dp[n-1][amount];
+        int ans= prev[amount];
         if(ans>=1e9) return -1;
         else return ans;
     }
