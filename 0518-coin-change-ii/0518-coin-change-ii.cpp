a1595d776443fb348bf6b1f465e3e230debe3e3b
Vidushi Singhal <104440693+21vidushi@users.noreply.github.com>
2024-05-03 14:40:10 +0530

    Time: 5 ms (78.77%), Space: 19.9 MB (32.64%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
+    int find(int amount, vector<int>& coins,int i, vector<vector<int>>&dp){
+        if(amount==0) return 1;
+        if(amount<0) return 0;
+        if(i==0){
+            if(amount%coins[i]==0) return 1;
+            else return 0;
+        }
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        int pick=0;
+        int nott=find(amount,coins,i-1,dp);
+        if(coins[i]<=amount){
+            pick=find(amount-coins[i],coins,i,dp);
+        }
+        return dp[i][amount]=pick+nott;
+    }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-       vector<int> prev(amount+1,0);
-        vector<int> curr(amount+1,0);
-        for(int i=0;i<=amount;i++){
-            prev[i]=(i%coins[0]==0);
-        }
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=amount;j++){
-                int nott=prev[j];
-                int pick=0;
-                if(coins[i]<=j){
-                    pick=curr[j-coins[i]];
-                }
-                curr[j]=pick+nott;
-            }
-            prev=curr;
-        }
-        return prev[amount];
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        return find(amount,coins,n-1,dp);
     }
 };
