ec00900b0690cca0e5f3c9085697611c85c3bf24
Vidushi Singhal <104440693+21vidushi@users.noreply.github.com>
2024-05-03 14:43:00 +0530

    Time: 19 ms (24.13%), Space: 19.6 MB (58.20%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -17,7 +17,22 @@ public:
     }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return find(amount,coins,n-1,dp);
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        for(int i=0;i<n;i++){
+            dp[i][0]=1;
+        }
+        for(int t=1;t<=amount;t++){
+            if(t%coins[0]==0) dp[0][t]=1;
+        }
+        for(int i=1;i<n;i++){
+            for(int t=1;t<=amount;t++){
+                int pick=0;
+                int nott=dp[i-1][t];
+                if(coins[i]<=t) pick=dp[i][t-coins[i]];
+                dp[i][t]=pick+nott;
+            }
+        }
+        
+        return dp[n-1][amount];
     }
 };
