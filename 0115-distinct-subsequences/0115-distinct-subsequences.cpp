d271a18d872c3f199ca30ba6d1417cc7d6bea6aa
Vidushi Singhal <104440693+21vidushi@users.noreply.github.com>
2024-05-06 17:25:20 +0530

    Time: 16 ms (67.87%), Space: 31.4 MB (33.11%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -8,8 +8,10 @@ public:
         }
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s[i-1]==t[j-1]) dp[i][j]=dp[i-1][j-1] +dp[i-1][j];
-                else dp[i][j]= dp[i-1][j];
+                double match=0,nott=0;
+                if(s[i-1]==t[j-1]) match=dp[i-1][j-1] +dp[i-1][j];
+                else nott= dp[i-1][j];
+                dp[i][j]=match+nott;
             }
         }
         
