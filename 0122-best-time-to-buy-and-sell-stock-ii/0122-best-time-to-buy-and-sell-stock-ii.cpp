5ce8fad2cf4835977a0da0131e20dd41dffbfc92
Vidushi Singhal <104440693+21vidushi@users.noreply.github.com>
2024-05-06 22:25:06 +0530

    Time: 0 ms (100.00%), Space: 15.7 MB (25.41%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -17,20 +17,21 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,0));
-        dp[n-1][1]=prices[n-1];
+       vector<int>prev(2,0),curr(2,0);
+        prev[1]=prices[n-1];
         for(int i=n-2;i>=0;i--){
             for(int buy=0;buy<=1;buy++){
                 int profit=0;
                 if(buy==0){
-                    profit=max(profit,max(-prices[i]+dp[i+1][1],dp[i+1][0]));
+                    profit=max(profit,max(-prices[i]+prev[1],prev[0]));
                 }
                 if(buy==1){
-                    profit=max(profit,max(prices[i]+dp[i+1][0],dp[i+1][1]));
+                    profit=max(profit,max(prices[i]+prev[0],prev[1]));
                 }
-                dp[i][buy]=profit;
+                curr[buy]=profit;
             }
+            prev=curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
