1b308a953207467ee340f351d03a1808e0df0d8c
Vidushi Singhal <104440693+21vidushi@users.noreply.github.com>
2024-05-08 00:17:21 +0530

    Time: 81 ms (48.87%), Space: 12.9 MB (44.91%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    int find(vector<int>&nums,int n,int i,int prev, vector<vector<int>>&dp){
-        if(i==n) return 0;
-        if(dp[i][prev+1]!=-1) return dp[i][prev+1];
-        int pick=0;
-        int nott=find(nums,n,i+1,prev,dp);
-        if(prev==-1||nums[prev]<nums[i]){
-            pick=1+ find(nums,n,i+1,i,dp);
-        }
-        return dp[i][prev+1]=max(pick,nott);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-       vector<int>before(n+1,0),curr(n+1,0);
-        for(int i=n-1;i>=0;i--){
-            for(int prev=i-1;prev>=-1;prev--){
-                int pick=0;
-                int nott=before[prev+1];
-                if(prev ==-1||nums[prev]<nums[i]){
-                    pick=1+ before[i+1];
+        vector<int>dp(n,1);
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(nums[prev]<nums[i]){
+                    dp[i]=max(dp[i],dp[prev]+1);
                 }
-                curr[prev+1]=max(pick,nott);
             }
-            before=curr;
         }
-        return before[0];
+        int maxi=-1;
+        for(int i=0;i<n;i++){
+            maxi=max(maxi,dp[i]);
+        }
+        return maxi;
     }
 };
